b9557e3bee22d1ebcecab383cb56e525d261fe2a
Ac-ayush <100469226+Ac-ayush@users.noreply.github.com>
2022-07-06 16:26:17 +0530

    Update 2. Preorder Traversal Tree.cpp

--- Day 18/2. Preorder Traversal Tree.cpp
+++ Day 18/2. Preorder Traversal Tree.cpp
@@ -1,15 +1,15 @@
-void inorder(TreeNode* root , vector<int> &in)
+void preorder(TreeNode* root , vector<int> &in)
 {
     if(root == NULL)return ;
     
     in.push_back(root->data);
-    inorder(root->left,in);
-    inorder(root->right,in); 
+    preorder(root->left,in);
+    preorder(root->right,in); 
 }
 
 vector<int> getInOrderTraversal(TreeNode *root)
 {
     vector<int> ans ; 
-    inorder(root,ans);
+    preorder(root,ans);
     return ans ;
 }
