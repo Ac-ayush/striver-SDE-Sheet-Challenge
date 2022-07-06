31fbcfaf54fa8d44899e2981bb88f70f95dce2eb
Ac-ayush <100469226+Ac-ayush@users.noreply.github.com>
2022-07-06 16:26:56 +0530

    Update 3. Postorder Traversal Tree.cpp

--- Day 18/3. Postorder Traversal Tree.cpp
+++ Day 18/3. Postorder Traversal Tree.cpp
@@ -2,8 +2,8 @@ void postorder(TreeNode* root , vector<int> &in)
 {
     if(root == NULL)return ;
     
-    inorder(root->left,in);
-    inorder(root->right,in);
+    postorder(root->left,in);
+    postorder(root->right,in);
     in.push_back(root->data);
 }
 
